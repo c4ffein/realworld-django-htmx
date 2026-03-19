f6f5876c9103e52bcadb65ea829f85ed51a6a8da
c4ffein <c4ffein@gmail.com>
2026-03-19 17:22:39 +0100

    init: context processor helper

---
+++ helpers/context_processors.py
@@ -0,0 +1,27 @@
+import json
+
+from helpers.jwt_utils import create_jwt_token
+
+
+def conduit_debug(request):
+    """Context processor that provides data for window.__conduit_debug__ interface."""
+    if request.user.is_authenticated:
+        token = request.session.get("jwt_token")
+        if not token:
+            token = str(create_jwt_token(request.user, request))
+            request.session["jwt_token"] = token
+        user_data = {
+            "username": request.user.username,
+            "email": request.user.email,
+            "bio": request.user.bio or None,
+            "image": request.user.image or None,
+            "token": token,
+        }
+        return {
+            "conduit_user_json": json.dumps(user_data),
+            "conduit_token": token,
+        }
+    return {
+        "conduit_user_json": "null",
+        "conduit_token": "",
+    }
