9e100719b4a8f4cacb39fc0323211823fd2118a2
c4ffein <c4ffein@gmail.com>
2026-03-20 17:07:02 +0100

    fix: return 403 on unauthorized comment delete instead of silent 200

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,5 +1,6 @@
 from articles.models import Article
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
@@ -25,8 +26,9 @@ def comment_create_view(request, slug):
 def comment_delete_view(request, slug, comment_id):
     article = get_object_or_404(Article, slug=slug)
     comment = get_object_or_404(Comment, id=comment_id, article=article)
-    if comment.author == request.user or article.author == request.user:
-        comment.delete()
+    if comment.author != request.user and article.author != request.user:
+        return HttpResponseForbidden()
+    comment.delete()
     if is_htmx(request):
         comments = article.comment_set.select_related("author").order_by("-created")
         return render(request, "partials/comment_list.html", {"comments": comments, "article": article})
